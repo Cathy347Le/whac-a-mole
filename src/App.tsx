c5a87bfcf5bfd1d77a2e8042269e622d649afcc0
Cathy Le <cathy347le@gmail.com>
2023-08-16 22:52:04 -0400

    refactor

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,15 @@ import './App.css';
 import { Hole } from './Components/Hole';
 
 export const App = () => {
+  const Holes = Array(6)
+    .fill(true)
+    .map((item, index) => <Hole key={index} id={index + 1} />);
   return (
     <div className="App">
       <header>
         <h1>WHAC-A-MOLE!</h1>
       </header>
-      <div className="game">
-        {Array(6)
-          .fill(true)
-          .map((item, index) => (
-            <Hole key={index} id={index + 1} />
-          ))}
-      </div>
+      <div className="game">{Holes}</div>
     </div>
   );
 };
